b55b218a53c3c94f7f7272aa0122b06b491dca61
ZachLofquist <zlofquis@gmail.com>
2019-09-27 12:04:38 -0700

    Third Times the Charm

--- lab1.py
+++ lab1.py
@@ -3,16 +3,16 @@ def max_list_iter(int_list):  # must use iteration not recursion
    """finds the max of a list of numbers and returns the value (not the index)
    If int_list is empty, returns None. If list is None, raises ValueError"""
 
-   if int_list == None:
+   if int_list == None:                  #If the list doesn't exist a ValueError will be raised
       raise ValueError
-   elif len(int_list) == 0:
+   elif len(int_list) == 0:              #If there is nothing in the list then None is returned
        return None
    else:
-       max = int_list[0]
-       for val in int_list:
-           if max < val:
+       max = int_list[0]                 #Sets the max to the first value in the list
+       for val in int_list:              #Goes through list comparing max to current value
+           if max < val:                 #If the current value is greater than max then the max is changed to be the current value
               max = val
-   return max
+   return max                            #Returns the max of the list
 
    pass
 
@@ -20,20 +20,20 @@ def max_list_iter(int_list):  # must use iteration not recursion
 def reverse_rec(int_list):   # must use recursion
    """recursively reverses a list of numbers and returns the reversed list
    If list is None, raises ValueError"""
-   if int_list == None:
+   if int_list == None:                               #If the list doesn't exist a ValueError is raised
       raise ValueError
-   if len(int_list) <= 1:
+   if len(int_list) <= 1:                             #If the length if the list is 1 or less the list is returned as it is the reverse already
       return int_list
-   if type(int_list[1]) != list:
-      int_list = [int_list, [len(int_list)]]
-      int_list[1][0] = int_list[0][-1]
-      del int_list[0][-1]
-   int_list[1].append(int_list[0][-1])
-   del int_list[0][-1]
-   if int_list[0]== []:
+   if type(int_list[1]) != list:                      #Checks to see if the second element in the list is a list
+      int_list = [int_list, [len(int_list)]]          #Makes the list into a list containing two lists. THe first being the original and the second an empty list
+      int_list[1][0] = int_list[0][-1]                #Sets the first element of the second list to the last element of the original list
+      del int_list[0][-1]                             #Deletes the the last element of the original list
+   int_list[1].append(int_list[0][-1])                #Adds the last element of the original list to the end of the second list
+   del int_list[0][-1]                                #Deletes the last element of the original list
+   if int_list[0]== []:                               #If the original list is empty then the list become the second list which is the original list reversed
        int_list = int_list[1]
-       return int_list
-   return reverse_rec(int_list)
+       return int_list                                #Returns the reversed list
+   return reverse_rec(int_list)                       #Recursively calls the function to redo the process
 
 
    pass
@@ -42,28 +42,22 @@ def reverse_rec(int_list):   # must use recursion
 def bin_search(target, low, high, int_list):  # must use recursion
    """searches for target in int_list[low..high] and returns index if found
    If target is not found returns None. If list is None, raises ValueError """
-   if int_list == None:
+   if int_list == None:                               #If the list doesn't exist a ValueError is raised
        raise ValueError
-   mid = (low + high) // 2
-   if len(int_list) == 1 and int_list[0] != target:
-       return None
-   if target not in int_list:
-       return None
-   if int_list[mid] == target:
+   mid = (low + high) // 2                            #Sets mid to the midpoint of high and low
+   if int_list[mid] == target:                        #If the mid is equal to the target to mid is returned
        return mid
-   if int_list[mid] > target:
-       high = mid
+   if mid == high and mid == low:                     #If the mid is equal to the high and the low then None is returned since it was already checked against the target
+       return None
+   if int_list[mid] > target:                         #Checks if the mid value of the list is greater than the target
+       high = mid - 1                                 #The high value is changed to 1 below the mid value
+       if high < 0:                                   #If the high value is negative it is changed back to 0
+           high = 0
    else:
-       low = mid
-   return bin_search(target, low, high, int_list)
+       low = mid + 1                                  #The low value is set to 1 above the mid value
+   return bin_search(target, low, high, int_list)     #Recursively calls the function to repeat the process
 
 
    pass
 
 
-
-int_list = [2, 3, 6, 9, 14, 21, 22, 23, 34, 35, 45, 46, 47, 48]
-
-#x = max_list_iter(list_lint)
-y = reverse_rec([1,2,3]),
-z = bin_search(22, 0, len(int_list) - 1, int_list)
--- location_tests.py
+++ location_tests.py
@@ -3,14 +3,55 @@ from location import *
 
 class TestLab1(unittest.TestCase):
 
+    #Tests if location, SLO, is string represented correctly
     def test_repr(self):
         loc = Location("SLO", 35.3, -120.7)
         self.assertEqual(repr(loc),"Location('SLO', 35.3, -120.7)")
 
+    # Tests if location, Paris, is string represented correctly
     def test_repr2(self):
         loc = Location("Paris", 48.9, 2.4)
-        self.assertEqual(repr(loc),"Location('SLO', 35.3, -120.7)")
-    # Add more tests!
+        self.assertEqual(repr(loc),"Location('Paris', 48.9, 2.4)")
+
+    # Tests if location, Seattle, is string represented correctly
+    def test_repr3(self):
+        loc = Location("Seattle", 47.6, -122.3)
+        self.assertEqual(repr(loc),"Location('Seattle', 47.6, -122.3)")
+
+    # Tests if location, Melbourne, is string represented correctly
+    def test_repr4(self):
+        loc = Location("Melbourne", -37.8, 144.9)
+        self.assertEqual(repr(loc),"Location('Melbourne', -37.8, 144.9)")
+
+    # Tests if location, Chiang Mai, is string represented correctly
+    def test_repr5(self):
+        loc = Location("Chiang Mai", 18.8, 98.9)
+        self.assertEqual(repr(loc),"Location('Chiang Mai', 18.8, 98.9)")
+
+    # Tests if SLO is equivalent to SLO
+    def test_eq(self):
+        loc1 = Location("SLO", 35.3, -120.7)
+        loc2 = Location("SLO", 35.3, -120.7)
+        self.assertEqual(loc1, loc2)
+
+    #Tests to check that SLO is not equivalent to Paris
+    def test_eq2(self):
+        loc1 = Location("SLO", 35.3, -120.7)
+        loc2 = Location("Paris", 48.9, 2.4)
+        self.assertNotEqual(loc1, loc2)
+
+    #Tests to check that Seattle equals Seattle
+    def test_eq3(self):
+        loc1 = Location("Seattle", 47.6, -122.3)
+        loc2 = Location("Seattle", 47.6, -122.3)
+        self.assertEqual(loc1, loc2)
+
+    #Checks that Melbourne is not equivalent to Chiang Mai
+    def test_eq4(self):
+        loc1 = Location("Melbourne", -37.8, 144.9)
+        loc2 = Location("Chiang Mai", 18.8, 98.9)
+        self.assertNotEqual(loc1, loc2)
+
 
 if __name__ == "__main__":
         unittest.main()
